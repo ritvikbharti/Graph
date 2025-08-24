df179131aa416d515221c825568b71653117fc4c
Ritvik Bharti <bhartiritvik1000@gmail.com>
2025-08-25 00:15:13 +0530

    kahn's revise

---
+++ kahnsAlgo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<queue> 
+using namespace std;
+
+
+
+int main(){
+    vector<int> adj[] = {};
+    vector<vector<int>> edges = {};
+    int n = 0;
+    vector<int> inDegree(n,0);
+    for(auto pair : edges){
+        int u = pair[0];
+        int v = pair[1];
+        adj[u].push_back(v);
+        inDegree[v]++;
+
+    }
+    queue<int> q;
+
+    for(int i = 0;i<n;i++){
+        if(inDegree[i] == 0) q.push(i);
+    }
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        for(int adjNode : adj[frontNode] ){
+            inDegree[adjNode]--;
+            if(!inDegree[adjNode] ) q.push(adjNode);
+        }
+    }
+
+
+}
